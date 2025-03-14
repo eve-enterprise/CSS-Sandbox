7f52e6d525dff0faf5436cb5092a37a13473eb53
ezeppa <zeppaeven.dev@gmail.com>
2025-03-14 21:49:20 +0100

    End cursor 1

--- cursor/1/js/script.js
+++ cursor/1/js/script.js
@@ -1,4 +1,22 @@
 // Fichier JavaScript principal
 document.addEventListener("DOMContentLoaded", function() {
-    console.log("Bienvenue sur mon site !");
+	document.addEventListener("mousemove", (e) => {
+		const cursor = document.querySelector(".cursor");
+		cursor.style.left = `${e.clientX}px`;
+		cursor.style.top = `${e.clientY}px`;
+	
+		const hoveredElement = document.elementFromPoint(e.clientX, e.clientY);
+	
+		if (hoveredElement) {
+			const ariaLabel = hoveredElement.getAttribute("aria-label");
+			if (ariaLabel) {
+				cursor.innerHTML = ariaLabel;
+				cursor.style.scale = 1;
+			} else {
+				cursor.style.scale = 0;
+			}
+		}
+	});
+	
 });
+
